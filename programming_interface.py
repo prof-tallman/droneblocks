09468b3e3114f99ecce050119556727b9ee220da
JP_Vela <jangofett122003@gmail.com>
2025-02-10 13:59:17 -0800

    Trying to get screen to show up

--- programming_interface.py
+++ programming_interface.py
@@ -1,15 +1,32 @@
 # Importing the library
 import pygame
  
-# Initializing Pygame
-pygame.init()
  
-# Initializing surface
-surface = pygame.display.set_mode((400,300))
- 
-# Initializing Color
-color = (255,0,0)
- 
-# Drawing Rectangle
-pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60))
-pygame.display.flip()
+class Interface:
+    def __init__(self):
+        # Initializing Pygame
+        pygame.init()
+        self.running = True
+
+        self.SIZE = (900, 700)
+        self.screen = pygame.display.set_mode(self.SIZE)
+        self.background_color = (0, 0, 50)
+
+
+    def run(self):
+        while self.running:
+            self.draw()
+
+        pygame.quit()
+
+
+    def draw(self):
+        self.screen.fill(self.background_color)
+
+        pygame.draw.rect(self.screen, (100,100,0), pygame.Rect(30, 30, 60, 60))
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    interface = Interface()
+
+    interface.run()
