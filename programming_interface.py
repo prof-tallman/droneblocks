6a25d825aa7273311c6afec2d481ecadeae70279
Andrew Shiroma <amshiroma@zoho.com>
2025-02-12 21:36:46 -0800

    added all blocks and placed them in position

--- programming_interface.py
+++ programming_interface.py
@@ -11,12 +11,20 @@ class Interface:
         self.SIZE = (1280, 720)
         self.screen = pygame.display.set_mode(self.SIZE)
         self.background_color = (0, 0, 50)
-
         
         self.blocks = [
-            Block(50,50, 'forward')
+            Block(50, 300, 'rotate_ccw'),
+            Block(150, 300, 'forward'),
+            Block(250, 300, 'rotate_cw'),
+            Block(50, 400, 'left'),
+            Block(150, 400, 'backward'),
+            Block(250, 400, 'right'),
+            Block(50, 500, 'up'),
+            Block(150, 500, 'hover'),
+            Block(250, 500, 'down'),
+            Block(100, 600, 'takeoff'),
+            Block(200, 600, 'land')
         ]
-
 
     def run(self):
         while self.running:
@@ -49,28 +57,25 @@ class Interface:
         # pygame.display.flip()
 
 
-
-
 class Block:
-    def __init__(self, x, y, action):
+    def __init__(self, x: int, y: int, action: str):
+        """
+        Initializes a new pygame rectangle object.
         
-        # Below is a quick explanation for the pygame.Rect() button class, 
-        # param x: x position of the button
-        # param y: y position of the button
-        # param width: width of the button
-        # param height: height of the button
-        # param color: color of the button
-        # param hover_color: color of the button when hovered
-        # param text: text to display on the button
-        # param font: font of the text
-        # param text_color: color of the text
-        # param action: function to call when the button is clicked
+        Parameters:
+            x (int): The x-coordinate of the object's position.
+            y (int): The y-coordinate of the object's position.
+            action (str): The text to be displayed on the object.
+        """
         color = (255,255,255)
 
-        self.width, self.height = 100, 50
+        self.width, self.height = 75, 75
 
         self.surface = pygame.Surface((self.width, self.height), pygame.SRCALPHA)  # Enables transparency
         self.surface.fill(color)  # Fill the rectangle with green
+
+        temp_font = pygame.font.SysFont('Arial', 14).render(action, True, (0, 0, 0))
+        self.surface.blit(temp_font, (5, 5))
 
         self.action = action
 
